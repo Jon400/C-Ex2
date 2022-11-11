1b773735c57387c31d099f9dba47bf1221642364
Jon400 <jonatan250199@gmail.com>
2022-11-11 22:57:42 +0200

    Initial commit

---
+++ C-Ex1/basicClassification.c
@@ -0,0 +1,56 @@
+#include "NumClass.h"
+#include <math.h>
+
+// Implemetnation of 'isPrime' function declaration from NumClass.h
+int isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return 0;
+    }
+    else if (num == 2)
+    {
+        return 1;
+    }
+    else if (num % 2 == 0)
+    {
+        return 0;
+    }  
+    else
+    {
+        int x;
+        int sqrt_num = sqrt(num);
+        int step_size = 2;
+        for (x = 3; x <= sqrt_num; x += step_size){
+            if (num % x == 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+// Implemetnation of 'isStrong' function declaration from NumClass.h
+int isStrong(int num)
+{
+    int temp_num = num;
+    int fact;
+    int x;
+    int res = 0;
+
+    if (temp_num <= 0){
+        return 0;
+    }
+
+    while (temp_num > 0)
+    {
+        fact = 1;
+        for (x = 2; x <= temp_num % 10; x++)
+        {
+            fact = fact * x;
+        }
+        res += fact;
+        temp_num = temp_num / 10;
+    }
+    return (res == num);
+}
---
+++ C-Ex1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+void main() {
+    printf(" %d ", isStrong(165));
+}
