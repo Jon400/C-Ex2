5d6595cd473734227912cbcbca209a071b8d3a6e
Jon400 <jonatan250199@gmail.com>
2022-11-14 20:46:05 +0200

    directory fix

---
+++ basicClassification.c
@@ -0,0 +1,57 @@
+#include "NumClass.h"
+
+// Implemetnation of 'isPrime' function declaration from NumClass.h
+int isPrime(int num)
+{
+    if (num < 1)
+    {
+        return 0;
+    }
+    else if (num == 2 || num == 1)
+    {
+        return 1;
+    }
+    else if (num % 2 == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        int x;
+        int step_size = 2;
+        for (x = 3; x < num; x += step_size)
+        {
+            if (num % x == 0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+// Implemetnation of 'isStrong' function declaration from NumClass.h
+int isStrong(int num)
+{
+    int temp_num = num;
+    int fact;
+    int x;
+    int res = 0;
+
+    if (temp_num <= 0)
+    {
+        return 0;
+    }
+
+    while (temp_num > 0)
+    {
+        fact = 1;
+        for (x = 2; x <= temp_num % 10; x++)
+        {
+            fact = fact * x;
+        }
+        res += fact;
+        temp_num = temp_num / 10;
+    }
+    return (res == num);
+}
---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+int main() {
+    int start_num, end_num;
+    int num;
+    int res;
+
+    res = 1;
+    printf("Please enter your range: ");
+    res = scanf("%d%d",&start_num, &end_num);
+    if(res == 0 || res == 26){
+        printf("input is invalid");
+        return 0;
+    }
+
+
+    // Print all armstong numbers in the given range
+    printf("The Armstrong numbers are:");
+    for (num = start_num; num <= end_num; num++)
+    {
+        if(isArmstrong(num)){
+            printf(" %d", num);
+        }
+    }
+    printf("\n");
+
+    // Print all palindrome numbers in the given range
+    printf("The Palindromes are:");
+    for (num = start_num; num <= end_num; num++)
+    {
+        if(isPalindrome(num)){
+            printf(" %d", num);
+        }
+    }
+    printf("\n");
+
+    // Print all prime numbers in the given range
+    printf("The Prime numbers are:");
+    for (num = start_num; num <= end_num; num++)
+    {
+        if(isPrime(num)){
+            printf(" %d", num);
+        }
+    }
+    printf("\n");
+
+    // Print all strong numbers in the given range
+    printf("The Strong numbers are:");
+    for (num = start_num; num <= end_num; num++)
+    {
+        if(isStrong(num)){
+            printf(" %d", num);
+        }
+    }
+    printf("\n");
+
+    // Terminator
+    return 0;
+}
